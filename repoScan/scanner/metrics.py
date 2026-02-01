94f9624d384bdae30fffef152e785b8849ac0e8b
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2026-02-01 04:35:29 +0000

    fix: function name for language metrics

--- repoScan/scanner/metrics.py
+++ repoScan/scanner/metrics.py
@@ -8,6 +8,7 @@ from ..utility.helpers import average_lines_per_file
 from ..utility.helpers import average_lines_per_file
 from ..utility.helpers import average_dependencies_per_file
 from ..utility.helpers import largest_file_by_lines
+from ..utility.helpers import total_dependencies
 
 def generate_metrics(file_path, size_report, dep_report):
     return {
@@ -22,5 +23,5 @@ def generate_metrics(file_path, size_report, dep_report):
             "max_file": file_with_most_dependencies(dep_report),
             "average": average_dependencies_per_file(dep_report),
         },
-        "languages": language_breakdown(file_path),
+        "languages": language_metrics(file_path),
     }
