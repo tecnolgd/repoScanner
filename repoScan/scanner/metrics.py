326aaa978acac8eae842f8295bab92f70627ec12
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2026-02-02 01:53:38 +0000

    rename: dictionary names for metrics

--- repoScan/scanner/metrics.py
+++ repoScan/scanner/metrics.py
@@ -12,16 +12,16 @@ from ..utility.helpers import total_dependencies
 
 def generate_metrics(file_path, size_report, dep_report):
     return {
-        "files": {
+        "file_metrics": {
             "total": total_files(file_path),
             "total_lines": total_lines(size_report),
             "average_lines": average_lines_per_file(size_report),
             "largest_file": largest_file_by_lines(size_report),
         },
-        "dependencies": {
+        "dependency_metrics": {
             "total": total_dependencies(dep_report),
             "max_file": file_with_most_dependencies(dep_report),
             "average": average_dependencies_per_file(dep_report),
         },
-        "languages": language_metrics(file_path),
+        "language_metrics": language_metrics(file_path),
     }
