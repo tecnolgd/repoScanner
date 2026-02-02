b1c7335a934912bbcc4d126b6d9c5b51c7067567
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2026-02-02 01:32:51 +0000

    feat: implemented key-value print function

--- repoScan/reports/terminalReports.py
+++ repoScan/reports/terminalReports.py
@@ -1,7 +1,11 @@
 #terminalReports.py
 
-
+#section printer
 def print_section(title):
     print("\n" + title)
     print("-" *len(title))
-    
+
+#key-value printer
+def print_kv(label, value, width=20):
+    print(f"{label.ljust(width)} : {value}")
+
