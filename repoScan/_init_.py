97b1b1e2dda6af407bb574ee67f1002eda975510
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2026-02-03 04:49:07 +0000

    feat: cleared test function

--- repoScan/_init_.py
+++ repoScan/_init_.py
@@ -1,22 +1,4 @@
 
-def loadTask(file_path):
-    tasks=[]
-
-    try:
-        with open(file_path,"r") as file:
-            for line in file:
-                task= line.strip()
-                if task:
-                    tasks.append(task)
-    except FileNotFoundError:
-        print(f"Error!. File {file_path} not found")
-    return tasks
-
-tasker=loadTask("data/testData.txt")
-print("Loaded tasks: ")
-for t in tasker:
-     print(t)
-
 
 
 
