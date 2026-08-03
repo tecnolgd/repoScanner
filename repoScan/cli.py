8e223732504ca6fbfeb2367c62b2d54d3920e68d
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2026-08-03 14:22:42 +0000

    chore: included some function calls imported from 'libcvault'

--- repoScan/cli.py
+++ repoScan/cli.py
@@ -2,6 +2,8 @@
 #cd /home/chief/projects/repoScanner && python3 -m repoScan.cli 2>&1 (exection command)
 
 import sys
+import libcvault
+
 from .analyzer.dependencyAnalyzer import dependency_analyzer
 from .scanner.dirScanner import dir_scanner
 from .analyzer.structureAnalyzer import structure_analyzer
@@ -10,6 +12,12 @@ from .scanner.metrics import generate_metrics
 from .reports.terminalReports import print_summary, print_raw_analysis
 from .reports.jsonReports import write_json_report
 from .utility.helpers import help_guide
+
+# Task to be completed
+
+# - Add all the required function calls for various features and flags
+# - Update the same in 'reposcan' bash script as well as update the required docs and help sections as well
+# - The next stage would be using reposcanner in piped instructions using streams
 
 def main():
     # Parse arguments
@@ -23,6 +31,17 @@ def main():
             mode = "stats"
         elif arg in ["--help", "-h"]:
             mode = "help"
+        elif arg in ["--sort"]:
+            mode = "sort"
+        elif arg in ["--max"]:
+            mode = "max"
+        elif arg in ["--search"]:
+            file_name = input("\t");
+            mode = "search"
+        elif arg in ["--lc"]:   
+            mode = "lc"
+        elif arg in ["--tbytes"]:
+            mode = "tbytes"
         else:
             root = arg
     
@@ -42,6 +61,19 @@ def main():
     elif mode == "help":
         help_guide()
         sys.exit(0)
+    # elif mode == "sort":
+        #sort function call
+    
+   # elif mode == "max":
+        #max byte function
+    # elif mode == "search":
+        #search for a file
+
+    # elif mode == "lc":
+        #line count
+    elif mode == "tbytes":
+        libcvault.populate_data(root); 
+        print('Total bytes:', libcvault.get_total_bytes())
     else:
         print("Invalid Mode!")
 
