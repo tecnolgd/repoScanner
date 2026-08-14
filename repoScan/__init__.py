2651c6373ef64d251fb5d36a345fa4e9251e199f
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2026-08-14 05:06:04 +0000

    chore: modified init file to include a fallback dev version

--- repoScan/__init__.py
+++ repoScan/__init__.py
@@ -1,3 +1,7 @@
-__version__ = "0.3.0b1"
+# Version is automatically managed by setuptools_scm
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "0.3.0.dev0"
 
 __all__ = ["__version__"]
